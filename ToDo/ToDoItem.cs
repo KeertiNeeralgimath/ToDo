4e61e8cced19fbcd9a3103489a2b1d5dd44ef241
premn <premn@LAPTOP-I9GU94KD>
2024-02-16 07:46:48 -0600

    Add project files.

---
+++ ToDo/ToDoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDo
+{
+    public class ToDoItem
+    {
+        public string TaskDescription { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+        public bool IsLate => DueDate < DateTime.Now && !IsCompleted;
+        public bool IsCompleted => CompletedDate.HasValue;
+    }
+}
